68fddd147c93125a563c303b62b6b0d149bc7d4f
zkx349113864 <zhang349113864@gmail.com>
2020-04-28 23:54:11 +0800

    新增代码

---
+++ Hello.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <afxwin.h>
+class CMyApp : public CWinApp
+{
+public:
+	virtual BOOL InitInstance();
+};
+
+class CMainWindow : public CFrameWnd
+{
+public:
+	CMainWindow();
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP();
+};
